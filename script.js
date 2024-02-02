47dfa55ccd7695edfac3e967c2dad58ab3231f12
laba0002 <laba0002@stud.kea.dk>
2024-02-02 10:15:19 +0100

    json og template

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Let go!");
+window.addEventListener("DOMContentLoaded", init);
+
+const beerURL = "https://api.punkapi.com/v2/beers";
+
+function init() {
+  fetch(beerURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      console.log("json", json);
+    });
+}
